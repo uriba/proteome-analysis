a4005657e7779c996f2d8ee410d5a91896c88766
Uri Barenholz <uri.b.mail@gmail.com>
2014-04-09 15:08:30 +0300

    updated python file to produce first graph, the file is reasonably structured. Started restructuring of data so that all calculated statistics will be on the same DataFrame and graphs will simply be extracted from the main DataFrame.

--- proteome-analysis.py
+++ proteome-analysis.py
@@ -4,13 +4,15 @@ import scipy.odr as od
 from pandas.io.parsers import read_csv
 from scipy.stats import gaussian_kde,linregress
 from Bio import SeqIO
+from matplotlib.pyplot import hist, savefig, figure
+
 
 #Initialization of basic data containers, gene annotation data, growth rates and cell volumes and selection of conditions to analyze.
 def uniprot_to_desc_dict():
     uni_konum_dict = {}
     uni_to_konum = read_csv('eco_uniprot_mapping.csv',sep='[\t:]',encoding='iso-8859-1',header = None, names = ['ko','bla','uniprot'])
-    for i,row in uni_to_eco.iterrows():
-         unidict[row['uniprot']]=row['ko']    
+    for i,row in uni_to_konum.iterrows():
+         uni_konum_dict[row['uniprot']]=row['ko']    
 
     #load the ko annotation tree:
     ko_annot_dict = {}
@@ -25,11 +27,13 @@ def uniprot_to_desc_dict():
              elif len(row) == 4:
                 component = row[-1]
              elif len(row) == 5:
-                annot_dict[row[-1]]=(cat,subcat,component)
+                ko_annot_dict[row[-1]]=(cat,subcat,component)
     uni_to_annot = {}
     for uni in uni_konum_dict:
         if uni_konum_dict[uni] == 'NotMapped':
             uni_to_annot[uni]=['NotMapped']
+        elif uni_konum_dict[uni] not in ko_annot_dict:
+	    uni_to_annot[uni]=['NotMapped']
         else:
             uni_to_annot[uni]=ko_annot_dict[uni_konum_dict[uni]]
     return uni_to_annot
@@ -53,39 +57,6 @@ def uniprot_to_offset():
             uniprot_to_location[uni]= locus_to_offset[uniprot_to_locus[uni]]
     return uniprot_to_location
 
-# define the growth rates:
-gr = {
-    u'chemostat \u00b5=0.12': 0.12, 
-    u'galactose':0.17, 
-    u'chemostat \u00b5=0.20':0.2, 
-    u'acetate':0.29, 
-    u'chemostat \u00b5=0.35':0.35,
-    u'glucosamine':0.39, 
-    u'pyruvate':0.4, 
-    u'glycerol':0.47,
-    u'fumarate':0.47, 
-    u'succinate':0.49, 
-    u'chemostat \u00b5=0.5':0.5, 
-    u'anaerobic' : 0.55, 
-    u'glucose': 0.6,
-    u'LB':1.61
-    }
-gr = pd.Series(gr)
-# define cell volumes:
-volumes = {u'glucose': 3.2, u'anaerobic' : 2.9, u'chemostat \u00b5=0.12': 2.1, u'chemostat \u00b5=0.5':2.6, u'galactose':1.9, u'acetate':2.4, u'glycerol':2.3,
-     u'LB':4.4,
-     u'glucosamine':2.9, u'fumarate':2.4, u'succinate':2.4, u'pyruvate':2.1, u'chemostat \u00b5=0.20':2.2, u'chemostat \u00b5=0.35':2.4}
-volumes = pd.Series(volumes)
-
-# first load the data from the file.
-# As the file was exported from Excel, it uses Excel's encoding.
-ecoli_data = read_csv('coli_data.csv',header=1,encoding='iso-8859-1')
-
-#Split the data loaded into two sets - weight and count.
-idx = ecoli_data.columns
-ecoli_data_weight = ecoli_data[idx[0:10].append(idx[29:])]
-ecoli_data_count = ecoli_data[idx[0:29]]
-
 # Define the list of conditions that will be relevant for the analysis, (and the description column):
 cond_list = [
     u'chemostat \u00b5=0.12',
@@ -103,102 +74,101 @@ cond_list = [
     u'glucose',
 #    u'LB'
 ]
+
+# define the growth rates:
+gr = {
+    u'chemostat \u00b5=0.12': 0.12, 
+    u'galactose':0.17, 
+    u'chemostat \u00b5=0.20':0.2, 
+    u'acetate':0.29, 
+    u'chemostat \u00b5=0.35':0.35,
+    u'glucosamine':0.39, 
+    u'pyruvate':0.4, 
+    u'glycerol':0.47,
+    u'fumarate':0.47, 
+    u'succinate':0.49, 
+    u'chemostat \u00b5=0.5':0.5, 
+    u'anaerobic' : 0.55, 
+    u'glucose': 0.6,
+    u'LB':1.61
+}
+gr = pd.Series(gr)
+gr = gr[cond_list]
+
+# define cell volumes:
+volumes = {
+    u'chemostat \u00b5=0.12': 2.1,
+    u'galactose':1.9,
+    u'chemostat \u00b5=0.20':2.2,
+    u'acetate':2.4,
+    u'chemostat \u00b5=0.35':2.4,
+    u'glucosamine':2.9,
+    u'pyruvate':2.1,
+    u'glycerol':2.3,
+    u'fumarate':2.4,
+    u'succinate':2.4,
+    u'chemostat \u00b5=0.5':2.6,
+    u'anaerobic' : 2.9,
+    u'glucose': 3.2,
+    u'LB':4.4
+}
+volumes = pd.Series(volumes)
+volumes = volumes[cond_list]
+
 #Define the text fields that will be relevant for the analysis:
 desc_list = ['Description','UP_AC']
 
-# Refine the DataFrames to include only these conditions (and the protein descriptions):
-count_cond = desc_list+cond_list
-ecoli_data_count = ecoli_data_count[count_cond]
+#Convert dataframe types to standart types for analysis
+def convert_types(df):
+    df = df[df != 'below LOQ']
+    df[cond_list] = df[cond_list].astype('float')
+    df[desc_list] = df[desc_list].astype('string')
+    return df
 
-#duplicate headers are modified by read_csv and include a trailing '.1' string in their name.
-weight_cond = desc_list+[x+'.1' for x in cond_list] 
-ecoli_data_weight = ecoli_data_weight[weight_cond]
+def get_coli_data(use_weight):
+    # As the file was exported from Excel, it uses Excel's encoding.
+    ecoli_data = read_csv('coli_data.csv',header=1,encoding='iso-8859-1')
 
-#rename the columns to remove the trailing '.1'
-ecoli_data_weight.columns = count_cond
+    #Split the data loaded into two sets - weight and count.
+    idx = ecoli_data.columns
+    ecoli_data_count = ecoli_data[idx[0:29]]
+    ecoli_data_weight = ecoli_data[idx[0:10].append(idx[29:])]
 
-#select the relevant data for analysis out of the two options:
-ecoli_data = ecoli_data_weight
+    # Refine the DataFrames to include only these conditions (and the protein descriptions):
+    count_cond = desc_list+cond_list
+    ecoli_data_count = ecoli_data_count[count_cond]
 
-#convert all columns, excluding 'Description', to floats, and 'Description' to string.
-ecoli_data_weight = ecoli_data_weight[ecoli_data_weight != 'below LOQ']
-ecoli_data_weight[cond_list] = ecoli_data_weight[cond_list].astype('float')
-ecoli_data_weight[desc_list] = ecoli_data_weight[desc_list].astype('string')
+    #duplicate headers are modified by read_csv and include a trailing '.1' string in their name.
+    weight_cond = desc_list+[x+'.1' for x in cond_list] 
+    ecoli_data_weight = ecoli_data_weight[weight_cond]
 
+    #rename the columns to remove the trailing '.1'
+    ecoli_data_weight.columns = count_cond
 
-ecoli_data = ecoli_data[ecoli_data != 'below LOQ']
-ecoli_data[cond_list] = ecoli_data[cond_list].astype('float')
-ecoli_data[desc_list] = ecoli_data[desc_list].astype('string')
+    #select the relevant data for analysis out of the two options:
+    if use_weight:
+        ecoli_data = ecoli_data_weight
+    else:
+        ecoli_data = ecoli_data_count
 
-#Normalize to get concentrations (Use the total protein weight as the normalizing factor to avoid errors in cell volume measurements)
-ecoli_data[cond_list] = ecoli_data[cond_list] / ecoli_data_weight[cond_list].sum() #volumes[cond_list]
+    #convert all data columns to floats, and description columns to strings.
+    ecoli_data_weight = convert_types(ecoli_data_weight)
+    ecoli_data = convert_types(ecoli_data)
 
-#get the ribosomal proteins.
-ecoli_data_values = ecoli_data.drop(desc_list,1)
-ecoli_data_values = ecoli_data_values.dropna()
+    #Normalize to get concentrations (Use the total protein weight as the normalizing factor to avoid errors in cell volume measurements)
+    ecoli_data[cond_list] = ecoli_data[cond_list] / ecoli_data_weight[cond_list].sum() #volumes[cond_list]
+    return ecoli_data
 
-ribs = ecoli_data['Description'].map(lambda x: 'ribosomal protein' in x)
-ribs_data = ecoli_data_values[ribs]
-ribs_avg = ribs_data.sum()
-gr = gr[ribs_avg.index]
-volumes = volumes[ribs_avg.index]
 ########################### Analysis of correlation of proteins to growth rate.
-xs = linspace(-1,1,100)
+uni_to_annot = uniprot_to_desc_dict()
+ecoli_data = get_coli_data(use_weight=True)
 
-covs = []
-catcovs = {}
-prots = {}
-high_cor_prots = {}
-threshold = 0.3
-hc_indices = []
-hc_prots = pd.DataFrame(columns = ecoli_data_values.columns)
+conc_data = ecoli_data
+conc_data = conc_data.dropna()
+conc_data['gr_cov']=conc_data[cond_list].apply(lambda x: x.corr(gr[cond_list]),axis=1)
+conc_data['rsq']=conc_data['gr_cov']**2
+conc_data['group']=conc_data.apply(lambda x: (uni_to_annot[x['UP_AC']])[0],axis=1)
 
-loc_prots = {0:[],1:[],2:[]}
-loc_indices = {0:[],1:[],2:[]}
-loc_data = {}
-
-for row_index, row in ecoli_data_values.iterrows():
-    desc = []
-    # calculate the correlation and add it to the statistics
-    cov = row.cov(gr)/(row.std() * gr.std())
-    # get the protein annotation information
-    eco = ecoli_data['UP_AC'][row_index]
-    uniprot = unidict[eco]
-    if eco in uniprot_to_location:
-	loc_prots[(uniprot_to_location[eco]/1600000)].append(cov)
-	loc_indices[(uniprot_to_location[eco]/1600000)].append(row_index)
-	loc_data[row_index]= uniprot_to_location[eco]
-    if uniprot in annot_dict:
-	desc = annot_dict[uniprot]
-	covs.append(cov)
-	# set the counters of all the annotation fields that this protein belong to.
-	for i in range(0,3):
-	    if desc[i] not in prots:
-	       prots[desc[i]]=0
-	       high_cor_prots[desc[i]]=0
-	       catcovs[desc[i]]=[]
-	    prots[desc[i]]+=1
-	    catcovs[desc[i]].append(cov)
-	    if cov > threshold: # threshold for gr correlated protein
-		high_cor_prots[desc[i]]+=1
-
-    if cov > 0.5:# and cov < 0.8:
-       hc_indices.append(row_index)
-       hc_prots = hc_prots.append(row)
-
-loc_data = pd.Series(loc_data)
-prots_num = len(covs)
-density = gaussian_kde(covs)
-plot(xs,density(xs),label='All proteins')
-title('Estimated distributions of correlation with growth rate by functional group')
-print "number of proteins compared: %d" % prots_num
-print "correlation by categories:"
-for i in loc_prots:
-    print '%d, %d' % (i,len(loc_prots[i]))
-
-for i in loc_prots:
-    density = gaussian_kde(loc_prots[i])
-    plot(xs,density(xs),label=str(i))
-
-legend(loc='upper left')
-savefig('some_subcategories_distributions.png')
+hist((conc_data[conc_data['group']!='NotMapped'])['gr_cov'],20)
+savefig('AnnotatedCorrWithGr.png')
+figure()
