1c87fd46d1af6d03ebd1fd6aaeb9d59318a2656d
Uri Barenholz <uri.b.mail@gmail.com>
2014-10-21 15:11:54 +0300

    more updates to presentation following ron comments

--- presentation.py
+++ presentation.py
@@ -5,7 +5,7 @@ from scipy.stats import gaussian_kde,linregress
 from scipy import stats
 from Bio import SeqIO
 from matplotlib.pyplot import hist, savefig, figure,figlegend,legend,plot,xlim,ylim,xlabel,ylabel,tight_layout,tick_params,subplot,subplots_adjust,text,subplots
-from numpy import linspace,ndarray,arange,sum,square,array,cumsum,ones
+from numpy import linspace,ndarray,arange,sum,square,array,cumsum,ones,mean
 from numpy.random import randn
 from analysis import *
 import matplotlib
@@ -14,19 +14,21 @@ from math import sqrt,isnan,log
 import random
 import plotly.plotly as py
 
-
+## a*0.5+b=1
+##
 py.sign_in("uri.barenholz", "hvi3ma3m30")
 
 figure(figsize=(5,3))
 
 p=subplot(111)
-xs = linspace(0.1,0.8,12)
-params = [(0.5,0.8),(1.5,0.5),(3,-0.3)]
+xs = linspace(0.1,0.9,9)
+params = [(0.5,0.75),(2,0),(0.25,0.375)]
 xlim(0,1)
+ylim(0,2)
 xlabel("Growth rate")
 ylabel("Protein concentration")
 for i,(a,b) in enumerate(params):
-    plot(xs,a*xs+b,'o',label='Protein %d' % (i+1))
+    plot(xs,(a*xs+b),'o',label='Protein %d' % (i+1))
 
 legend(loc=1, prop={'size':6},numpoints=1)
 tight_layout()
